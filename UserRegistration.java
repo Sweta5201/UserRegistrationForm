7fc9f834b0320f5ae4ac6cf6c2d18437f9ccc681
Sweta5201 <sweta65959@gmail.com>
2020-10-01 19:41:34 +0530

    [sweta] UC1 done - First name validation

---
+++ UserRegistration.java
@@ -0,0 +1,19 @@
+package com.practice.regexproblem;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your first name: ");
+        String firstName = sc.nextLine();
+        String pattern = "[A-Z]{1}[a-zA-Z]{2,}";
+        if(Pattern.matches(pattern, firstName))
+        	System.out.println("Valid first name");
+        else 
+        	System.out.println("Invalid first name!!!...Try again");
+        sc.close();
+	}
+
+}
