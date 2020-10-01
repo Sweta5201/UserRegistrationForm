92a2d9de8c16a34f19bd685b0d131d1e67c03d2f
Sweta5201 <71651178+Sweta5201@users.noreply.github.com>
2020-10-02 01:44:34 +0530

    Update UserRegistration.java
    [sweta] uc1 - indentation correction

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,4 +1,5 @@
 package com.practice.regexproblem;
+
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -7,13 +8,13 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your first name: ");
-        String firstName = sc.nextLine();
-        String pattern = "[A-Z]{1}[a-zA-Z]{2,}";
-        if(Pattern.matches(pattern, firstName))
-        	System.out.println("Valid first name");
-        else 
-        	System.out.println("Invalid first name!!!...Try again");
-        sc.close();
+                String firstName = sc.nextLine();
+                String pattern = "[A-Z]{1}[a-zA-Z]{2,}";
+                if(Pattern.matches(pattern, firstName))
+        	     System.out.println("Valid first name");
+               else 
+        	     System.out.println("Invalid first name!!!...Try again");
+               sc.close();
 	}
 
 }
