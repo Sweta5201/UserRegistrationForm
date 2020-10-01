a839905304267fd035b9c7f2ac225880c32a6f52
Sweta5201 <sweta65959@gmail.com>
2020-10-01 20:30:04 +0530

    [sweta] UC2 done - last name validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,4 +1,5 @@
 package com.practice.regexproblem;
+
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -7,13 +8,39 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your first name: ");
-        String firstName = sc.nextLine();
-        String pattern = "[A-Z]{1}[a-zA-Z]{2,}";
-        if(Pattern.matches(pattern, firstName))
-        	System.out.println("Valid first name");
-        else 
-        	System.out.println("Invalid first name!!!...Try again");
-        sc.close();
-	}
+		String firstName = sc.nextLine();
+		String pattern = "[A-Z]{1}[a-zA-Z]{2,}";
+		if (Pattern.matches(pattern, firstName)) {
+			System.out.println("Valid first name");
+			System.out.println("\nEnter your last name: ");
+			String lastName = sc.nextLine();
+			if (Pattern.matches(pattern, lastName)) {
+				System.out.println("Valid  last name");
+			} else {
+				System.out.println("Invalid last name!!!...Try again");
+			}
+			System.out.println("\nUser name is " + firstName + " " + lastName);
+		} else {
+			System.out.println("Invalid first name!!!...Try again");
+			do {
+				System.out.println("Enter your first name: ");
+				firstName = sc.nextLine();
+			} while (!Pattern.matches(pattern, firstName));
 
+			System.out.println("\nEnter your last name: ");
+			String lastName = sc.nextLine();
+			if (Pattern.matches(pattern, lastName)) {
+				System.out.println("Valid last name");
+			} else {
+				System.out.println("Invalid last name!!!...Try again");
+				do {
+					System.out.println("Enter your last name: ");
+					lastName = sc.nextLine();
+				} while (!Pattern.matches(pattern, lastName));
+				System.out.println("Valid  last name");
+				System.out.println("\nUser name is " + firstName + " " + lastName);
+				sc.close();
+			}
+		}
+	}
 }
